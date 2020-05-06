ce7274a5f3b6680396cd47620bbe200d9e0fadd6
shreyashgupta <shreyashgupta88@gmail.com>
2020-05-06 16:00:23 +0530

    Updates

---
+++ precache-manifest.140ea69d94f9fc85022e901d186d97b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5acacaf00b5cfadf82d447d7b085045",
+    "url": "/Robots/static/media/Blanka-Regular.d5acacaf.woff"
+  },
+  {
+    "revision": "7afafea27a891d641ba1156cbe0259eb",
+    "url": "/Robots/static/media/BATMAN.7afafea2.woff"
+  },
+  {
+    "revision": "a5c193ff50435e879254",
+    "url": "/Robots/static/js/runtime~main.a5c193ff.js"
+  },
+  {
+    "revision": "c26fb8a14aaa42fea5c2",
+    "url": "/Robots/static/js/main.c26fb8a1.chunk.js"
+  },
+  {
+    "revision": "0b0d6f14b8480df5f690",
+    "url": "/Robots/static/js/2.0b0d6f14.chunk.js"
+  },
+  {
+    "revision": "c26fb8a14aaa42fea5c2",
+    "url": "/Robots/static/css/main.2f8818cf.chunk.css"
+  },
+  {
+    "revision": "0b0d6f14b8480df5f690",
+    "url": "/Robots/static/css/2.003700ea.chunk.css"
+  },
+  {
+    "revision": "785c5c6af805e15c364f5e779d1b974e",
+    "url": "/Robots/index.html"
+  }
+];
